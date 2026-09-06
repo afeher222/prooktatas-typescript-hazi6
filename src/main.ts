35bbdd51ccda72bd3ff9c7478be5a112150ba890
Adam F <feher.adam@gmail.com>
2026-09-07 01:42:00 +0200

    Dekorátor logolás

--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,31 @@ import { TodoItem } from "./models/TodoItem";
 import { TodoList } from "./services/TodoList";
 import { Task, TaskWithDate, TodoType } from "./utils/TodoType";
 
-let todo1 = new TodoItem<Task>(1, "bevásárlás");
-let todo2 = new TodoItem<TaskWithDate>(2, {message: "házi feladat", 
-    dueDate: new Date("2026-09-06 23:00")});
-let todo3 = new TodoItem<Task>(3, "takarítás");
-let todo4 = new TodoItem<TaskWithDate>(4, {message: "vonatjegy", 
-    dueDate: new Date("2026-09-13 8:00")});
-let todo5 = new TodoItem<Task>(5, "főzés");
-
-let todos = [todo1, todo2, todo3, todo4, todo5]
-
+let todos = [
+    new TodoItem<Task>(1, "bevásárlás"),
+    new TodoItem<TaskWithDate>(2, {message: "házi feladat", 
+        dueDate: new Date("2026-09-06 23:00")}),
+    new TodoItem<Task>(3, "takarítás"),
+    new TodoItem<TaskWithDate>(4, {message: "vonatjegy", 
+        dueDate: new Date("2026-09-13 8:00")}),
+    new TodoItem<Task>(5, "főzés"),
+]
 
 let myTodos = new TodoList<TodoType>();
 for (let todo of todos){
     myTodos.addItem(todo);
 }
 
-console.log("Összes feladat:");
+console.log("\nÖsszes feladat:");
 myTodos.listItems();
 
-console.log("Törlés");
 myTodos.deleteItemById(3);
 
-console.log("Összes feladat:");
+console.log("\nÖsszes feladat:");
 myTodos.listItems();
 
-console.log('Határidős feladatok:');
+console.log("\nHatáridős feladatok:");
 myTodos.listItemsWithDate(true);
 
-console.log('Határidő nélküli feladatok:');
+console.log('\nHatáridő nélküli feladatok:');
 myTodos.listItemsWithDate(false);
--- src/services/TodoList.ts
+++ src/services/TodoList.ts
@@ -4,11 +4,13 @@ import { TodoType } from "../utils/TodoType";
 export class TodoList<T extends TodoType>{
     private items: Map<number, TodoItem<T>> = new Map();
 
+    @LogMethod("Add metódus felhívva")
     public addItem(item: TodoItem<T>) : void {
         this.items.set(item.id, item);
         console.log(`Todo hozzáadva: ${item.id}`);
     }
 
+    @LogMethod("Delete metódus felhívva")
     public deleteItemById(id: number) : void {
         if (this.items.has(id)){
             console.log(`Todo törölve: ID=${id}`);
@@ -35,4 +37,27 @@ export class TodoList<T extends TodoType>{
         console.log("---------------\n");     
     }        
     
+}
+
+/*
+function Logger(text: string) {    
+    function wrapper(target: any, context: ClassMethodDecoratorContext) {
+        function decorator(this: any, ...args: any[]){
+            console.log(text);
+            return target.apply(this, args);
+        }
+        return decorator;
+    };
+    return wrapper;
+}
+*/
+
+function LogMethod(text: string){
+    return function (target: any, propertyName: string, propertyDesciptor: PropertyDescriptor) {
+        const originalMethod = propertyDesciptor.value;
+        propertyDesciptor.value = function (...args: any[]) {            
+            console.log(text, JSON.stringify(args));
+            return originalMethod.apply(this, args);
+        };
+    }
 }
